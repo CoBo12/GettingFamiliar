280c36a393aa1679ba595dddb5f1d467fc9681a5
Corbin Bosley <corbinbosley@gmail.com>
2020-12-25 14:41:58 -0500

    Getting familiar with routing assignment

--- ControllersPractice/src/main/java/com/example/demo/DojoController.java
+++ ControllersPractice/src/main/java/com/example/demo/DojoController.java
@@ -1,5 +1,26 @@
 package com.example.demo;
 
-public class DojoController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+
+
+
+public class DojoController {
+	@RequestMapping("/{word}")
+	public String returnAwesome(@PathVariable("word") String word) {
+		return "The " + word + " is awesome!";
+	}
+
+	@RequestMapping("/{title}-dojo")
+	public String returnTitle(@PathVariable("title") String title) {
+		return title + " Dojo is located in Southern California";
+	}
+	@RequestMapping("/{location}")
+	public String returnLocation(@PathVariable("location") String location) {
+		return location + " dojo is the headquarters";
+	}
 }
